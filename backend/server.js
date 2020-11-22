ad6186cbea11e3a9d80eab610ccefc4eb7fa51f5
fahadshafiq.aiotize@gmail.com <f.shafiq575@gmail.com>
2020-11-22 16:05:48 +1100

    ok
    done

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,33 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
-);
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log("MongoDB database connection established successfully");
-})
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
 
-const exercisesRouter = require('./routes/exercises');
-const usersRouter = require('./routes/users');
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/politics", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-app.use('/exercises', exercisesRouter);
-app.use('/users', usersRouter);
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose is connected!!!!");
+});
+
+const exercisesRouter = require("./routes/exercises");
+const usersRouter = require("./routes/users");
+
+app.use("/exercises", exercisesRouter);
+app.use("/users", usersRouter);
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
